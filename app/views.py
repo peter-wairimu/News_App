3f438c62b7aae96844c04992d96180b50dfd3f6e
peter-wairimu <peter.wairimu@student.moringaschool.com>
2021-09-10 19:32:50 +0300

    defined the news function

--- app/views.py
+++ app/views.py
@@ -2,20 +2,6 @@ from flask import render_template
 from app import app
 from .request import get_movies
 
-
-# Views
-# @app.route('/')
-# def index():
-
-#     '''
-#     View root page function that returns the index page and its data
-#     '''
-
-#     title = 'Home - Welcome to The best Movie Review Website Online'
-#     return render_template('index.html', title = title)
-
-
-# @app.route('/movie/<movie_id>')
 @app.route('/')
 def index():
 
@@ -23,8 +9,14 @@ def index():
     View root page function that returns the index page and its data
     '''
 
-    # Getting popular movie
+    # Getting popular News
     popular_movies = get_movies()
-    print(popular_movies)
-    title = 'Home - Welcome to The best Movie Review Website Online'
-    return render_template('index.html', title = title,popular = popular_movies)
+    id = get_movies()
+    name = get_movies()
+    description = get_movies()
+    url = get_movies()
+    category = get_movies()
+    language = get_movies()
+    country = get_movies()
+    title = 'Home - Welcome to The best site for Your daily News'
+    return render_template('index.html', title = title,popular = popular_movies,id =id,name = name, description = description,url =url,category = category,language = language,country = country)
